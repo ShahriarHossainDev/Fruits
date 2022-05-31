a18f7afea638ae84ab2e43139233af0e28255a66
ShahriarHossainDev <shahriar.hossain.dev@gmail.com>
2022-06-01 03:22:44 +0600

    FruitCardView

---
+++ Fruits/View/FruitCardView.swift
@@ -0,0 +1,50 @@
+//
+//  FruitCardView.swift
+//  Fruits
+//
+//  Created by Shishir_Mac on 1/6/22.
+//
+
+import SwiftUI
+
+struct FruitCardView: View {
+    
+    // MARK: - Properties
+    
+    // MARK: - Body
+    
+    var body: some View {
+        ZStack {
+            VStack(spacing: 20) {
+                Image("blueberry")
+                    .resizable()
+                    .scaledToFit()
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 8, x: 6, y: 8)
+                
+                Text("Blueberry")
+                    .foregroundColor(Color.white)
+                    .font(.largeTitle)
+                    .fontWeight(.heavy)
+                    .shadow(color: Color(red: 0, green: 0, blue: 0, opacity: 0.15), radius: 2, x: 2, y: 2)
+                
+                Text("Blueberries are sweet, nutritious and wildly popular fruit all over the world.")
+                    .foregroundColor(Color.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.horizontal, 16)
+                    .frame(maxWidth: 480)
+            }//: VStack
+        } //: ZStack
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .center)
+        .background(LinearGradient(gradient: Gradient(colors: [Color("ColorBlueberryLight"), Color("ColorBlueberryDark")]), startPoint: .leading, endPoint: .trailing))
+        .cornerRadius(20)
+    }
+}
+
+// MARK: - Preview
+
+struct FruitCardView_Previews: PreviewProvider {
+    static var previews: some View {
+        FruitCardView()
+            .previewLayout(.fixed(width: 320, height: 640))
+    }
+}
---
+++ Fruits/View/StartButtonView.swift
@@ -0,0 +1,20 @@
+//
+//  StartButtonView.swift
+//  Fruits
+//
+//  Created by Shishir_Mac on 1/6/22.
+//
+
+import SwiftUI
+
+struct StartButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StartButtonView_Previews: PreviewProvider {
+    static var previews: some View {
+        StartButtonView()
+    }
+}
