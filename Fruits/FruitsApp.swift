3c8556982959a044bd66ce38b22e734ad7321cb7
ShahriarHossainDev <shahriar.hossain.dev@gmail.com>
2022-05-31 13:32:07 +0600

    Initial Commit

---
+++ Fruits/FruitsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FruitsApp.swift
+//  Fruits
+//
+//  Created by Shishir_Mac on 31/5/22.
+//
+
+import SwiftUI
+
+@main
+struct FruitsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
